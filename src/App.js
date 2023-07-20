c0ae26723fa2c9ed9eef9c302382592a127879f9
kunal786ks <ks445kunalsharma@gmail.com>
2023-07-20 16:41:24 +0530

    kunal

--- src/App.js
+++ src/App.js
@@ -79,7 +79,10 @@ function App() {
 
   return (
     <>
-      {complete && <div style={{ position: "absolute",borderRadius:'20px 20px 20px 20px', overflowY:"scroll",wordBreak:'break-all', boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",padding: "20px", display: "flex", flexDirection: "column", width: "30%", height: "100vh", backgroundColor: "beige" }}>
+      {complete && <div style={{ position:  "absolute",
+      borderRadius:'20px 20px 20px 20px', 
+      boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+      overflowY:"scroll",wordBreak:'break-all', padding: "20px", display: "flex", flexDirection: "column", width: "30%", height: "100vh", backgroundColor: "beige" }}>
         <div style={{
           display: 'flex',
           justifyContent: 'space-between'
